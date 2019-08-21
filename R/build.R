5d43ed741918dddf77328579bd4e385b122d8534
Anton Shirikov <shirickov@gmail.com>
2019-08-21 06:59:40 -0500

    update 1

---
+++ R/build.R
@@ -0,0 +1,61 @@
+# ----------------------------------------------------
+#   Setup file for Math Camp scripts
+#   Run this before anything else
+#   Working directory should ALREADY be the PROJECT ROOT
+# ----------------------------------------------------
+
+# should be in "math-camp" folder already
+library("here")
+
+# data stuff
+library("magrittr")
+library("tidyverse")
+
+# slides
+library("knitr")
+library("webshot")
+
+
+# --- Build slideshows -----------------------
+
+# this code automates the building of .rmd files into HTML
+# and then it automates the snapshotting of HTMLs into PDFs
+# (using webshot; decktape was a paaaaaaaiiiin)
+
+
+build_frame <- 
+  data_frame(stem = c("0-intro", 
+                      "1-pre-calc", 
+                      "2-linear-algebra", 
+                      "3-calculus", 
+                      "4-probability")) %>%
+  mutate(rmdfile = str_glue("lectures/{stem}/lecture-{stem}.rmd"),
+         htmlfile = str_glue("lectures/{stem}/lecture-{stem}.html"), 
+         htmlpath = str_glue("file://{normalizePath(here(htmlfile))}"),
+         pdfdest = str_glue("lectures/pdf/{stem}.pdf")) %>%
+  group_by(stem) %>%
+  nest() %>% 
+  print()
+
+
+
+# build RMD to HTML
+build_frame %$%
+  lapply(data, function(x) {
+   rmarkdown::render(input = here(x$rmdfile), 
+                     output_file = here(x$htmlfile),
+                     output_format = "xaringan::moon_reader")
+   print(str_glue("{x$rmdfile} done"))
+  })
+
+
+
+# make PDFs
+dir.create(here("lectures/pdf"))
+build_frame %$%
+  lapply(data, function(x) {
+   webshot(x$htmlfile, x$pdfdest, delay = 10)
+   print(str_glue("{x$pdfdest} successfully webshotted"))
+   beepr::beep(1)
+  })
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# math-camp-2019
+Resources for UW–Madison Political Science math camp, 2019
+
+Update
